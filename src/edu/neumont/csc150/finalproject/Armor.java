395f561f96e8ddf7ba8441491ad68ff4edcac4f8
Jonathan <JQuirk@student.neunmont.edu>
2017-03-01 13:32:36 -0700

    Create Armor class and Equip interface.
    Signed-off-by: Jonathan <JQuirk@student.neunmont.edu>

---
+++ src/edu/neumont/csc150/finalproject/Armor.java
@@ -0,0 +1,9 @@
+package edu.neumont.csc150.finalproject;
+
+public class Armor extends Item implements Equippable {
+
+	public Armor(String location) {
+		super(location);
+	}
+
+}
---
+++ src/edu/neumont/csc150/finalproject/Equippable.java
@@ -0,0 +1,5 @@
+package edu.neumont.csc150.finalproject;
+
+public interface Equippable {
+
+}
